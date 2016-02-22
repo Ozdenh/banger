c0c649a092c9c35fba19b777fea21e90c8618b26
pigdog13 <halavurt@mail.ru>
2016-02-22 13:26:32 +0000

    Created pages controller. Added various stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+#define root URL
+root 'pages#index'
+#define root for pages
+  get 'home' => 'pages#home' #override default routes
+  get 'profile' => 'pages#profile'
+  get 'explore' => 'pages#explore'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
